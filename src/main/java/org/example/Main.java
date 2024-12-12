ce0dae1a43b7c0307f8a57d8914ff887d664da7c
Moughite Hanafi <hanafi_moughite@yahoo.fr>
2024-12-12 11:34:13 +0100

    collection exercices testen

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        ExBasic exBasic=new ExBasic();
-        exBasic.AllMethode();
+//        ExBasic exBasic=new ExBasic();
+//        exBasic.AllMethode();
 
-//        ExCollections exCollections=new ExCollections();
-//        exCollections.allMethode();
+        ExCollections exCollections=new ExCollections();
+        exCollections.allMethode();
     }
 }
